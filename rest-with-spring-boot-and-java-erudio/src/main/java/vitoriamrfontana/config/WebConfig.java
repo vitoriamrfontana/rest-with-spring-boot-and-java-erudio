58d426352cc15b7606952280d4044b18986a1793
Vitória Maria Reis Fontana <07405773109@SEDUC.local>
2026-03-13 18:00:20 -0300

    Nova atualização

--- rest-with-spring-boot-and-java-erudio/src/main/java/vitoriamrfontana/config/WebConfig.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/vitoriamrfontana/config/WebConfig.java
@@ -11,10 +11,17 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+//
+//        configurer.favorParameter(true)
+//                .parameterName("mediaType")
+//                .ignoreAcceptHeader(true)
+//                .useRegisteredExtensionsOnly(false)
+//                .defaultContentType(MediaType.APPLICATION_JSON)
+//                    .mediaType("json", MediaType.APPLICATION_JSON)
+//                .mediaType("xml", MediaType.APPLICATION_XML);
 
-        configurer.favorParameter(true)
-                .parameterName("mediaType")
-                .ignoreAcceptHeader(true)
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                     .mediaType("json", MediaType.APPLICATION_JSON)
